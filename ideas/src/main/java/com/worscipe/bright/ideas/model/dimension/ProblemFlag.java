9011e7fbbc66087a28ef43c7bf79fc87ba94d2f1
Matthew Dowell <mbd06b@gmail.com>
2019-09-09 15:35:30 +0000

    more problem documentation

--- ideas/src/main/java/com/worscipe/bright/ideas/model/dimension/ProblemFlag.java
+++ ideas/src/main/java/com/worscipe/bright/ideas/model/dimension/ProblemFlag.java
@@ -5,6 +5,7 @@ public enum ProblemFlag {
 	CONFLICT, //conflicts with another ideaImpl - triggers Ranked Choice Voting bracket with the conflicting ideaImpl. 
 	DUPLICATE, //duplicate of other ideaImpl - triggers ideaImpl merge with branch lock
 	
+	CONTAGION, // has previously been identified as false or misleading by a reputable non-partisan institution
 	LAZY, // fails to demonstrate constructive value of the user Examples: memes, images, or quotes reshared
 	POOR_SOURCE, // relies on an information that wouldn't qualify in any qualified bibliography
 	FALSE_PREMISE, // relies on a objectively false premise that contradicts academically quantifiable consensus.
@@ -13,7 +14,7 @@ public enum ProblemFlag {
 	VIOLENT, // includes or describes an act of physical harm
 	HARASSMENT,  // targets any individual or a group of individuals
 	HATE, // does not reflect how someone would want to be subjected if roles were reversed
-	SPAM, // not interested in the subject, or presents a confilct of interest to the subject.
+	SPAM, // not interested in the subject, or is not in the interest of the participants as established by an independent feduciary
 	OTHER;
 	
 	ProblemFlag(){};
