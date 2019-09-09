0404e97860afcdd0a6a718e5244a72bd9419377a
Matthew Dowell <mbd06b@gmail.com>
2019-09-09 15:22:32 +0000

    some comments added to problem flags.

--- ideas/src/main/java/com/worscipe/bright/ideas/model/dimension/ProblemFlag.java
+++ ideas/src/main/java/com/worscipe/bright/ideas/model/dimension/ProblemFlag.java
@@ -4,13 +4,16 @@ public enum ProblemFlag {
 	NONE,
 	CONFLICT, //conflicts with another ideaImpl - triggers Ranked Choice Voting bracket with the conflicting ideaImpl. 
 	DUPLICATE, //duplicate of other ideaImpl - triggers ideaImpl merge with branch lock
-	IGNORANT,
-	EXPLICIT,
-	DISENGENUOUS,
-	VIOLENT,
-	HARASSMENT,
-	HATE,
-	SPAM,
+	
+	LAZY, // fails to demonstrate constructive value of the user Examples: memes, images, or quotes reshared
+	POOR_SOURCE, // relies on an information that wouldn't qualify in any qualified bibliography
+	FALSE_PREMISE, // relies on a objectively false premise that contradicts academically quantifiable consensus.
+	EXPLICIT, // adult content that would be generally disallowed without an accompaning academic premise or context
+	DISENGENUOUS, // overtly abuses context to manipulate an audience
+	VIOLENT, // includes or describes an act of physical harm
+	HARASSMENT,  // targets any individual or a group of individuals
+	HATE, // does not reflect how someone would want to be subjected if roles were reversed
+	SPAM, // not interested in the subject, or presents a confilct of interest to the subject.
 	OTHER;
 	
 	ProblemFlag(){};
