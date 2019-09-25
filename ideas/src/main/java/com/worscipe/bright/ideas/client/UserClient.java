49f8507ad12ffdeefc98f2a80547567f29b1af69
Mbd06b <mbd06b@gmail.com>
2019-09-25 11:07:00 -0500

    open feign artifact

--- ideas/src/main/java/com/worscipe/bright/ideas/client/UserClient.java
+++ ideas/src/main/java/com/worscipe/bright/ideas/client/UserClient.java
@@ -1,5 +1,16 @@
 package com.worscipe.bright.ideas.client;
 
-public class UserClient {
+import java.util.List;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "user-service")
+public interface UserClient {
+
+	@GetMapping("/idea/{ideaId}")
+	List<Long> findByIdea(@PathVariable("ideaId") Long ideaId);
+	
 }
