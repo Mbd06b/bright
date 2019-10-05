03392a9217ff6b6156027c55d063b89f91d68538
Mbd06b <mbd06b@gmail.com>
2019-10-05 08:33:06 -0500

    added spring-cloud dependencies to elections module

--- elections/src/main/java/com/worscipe/bright/elections/ElectionsApplication.java
+++ elections/src/main/java/com/worscipe/bright/elections/ElectionsApplication.java
@@ -2,8 +2,10 @@ package com.worscipe.bright.elections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ElectionsApplication {
 
 	public static void main(String[] args) {
