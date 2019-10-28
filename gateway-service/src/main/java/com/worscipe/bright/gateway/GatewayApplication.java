b600fdad19b45066aca63cc11d0c789f1dd0d1fc
Mbd06b <mbd06b@gmail.com>
2019-10-27 21:53:08 -0500

    package name refactor

--- gateway-service/src/main/java/com/worscipe/bright/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/worscipe/bright/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.worscipe.bright.services.gateway;
+package com.worscipe.bright.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
