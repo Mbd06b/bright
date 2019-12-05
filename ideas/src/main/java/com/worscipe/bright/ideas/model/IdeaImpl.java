13d5c427d6204590a92fe62fd6afa739d4ec3829
Mbd06b <mbd06b@gmail.com>
2019-12-04 21:41:45 -0600

    defining table name

--- ideas/src/main/java/com/worscipe/bright/ideas/model/IdeaImpl.java
+++ ideas/src/main/java/com/worscipe/bright/ideas/model/IdeaImpl.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -22,6 +23,7 @@ import javax.persistence.TemporalType;
 
 
 @Entity
+@Table (name = "idea")
 public class IdeaImpl implements Idea{
 
 
